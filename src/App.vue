1b32951275212d270f072e8e3b436abc168edc5f
Gaci <jugurthagaci24@gmail.com>
2023-02-01 21:32:45 +0100

    first upload

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Form from './components/Form.vue'
+
+</script>
+
+<template>
+    <div id="app" class="container py-5">
+		<h2 class="text-center">Calculer votre moyenne S3/Mathématique</h2>
+      	<Form />
+    </div>
+
+</template>
+
+<style scoped>
+
+</style>
