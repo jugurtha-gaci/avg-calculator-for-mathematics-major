d57692d09960f68be4d668e512e5c9c24042e56e
Jugurtha Gaci <88148092+gcyugurten@users.noreply.github.com>
2023-03-10 17:54:13 +0100

    Update Form.vue

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -24,13 +24,13 @@
                         <td v-if="index == 0" :rowspan="unite.modules.length" class="bg-light px-2">{{ unite.name }}</td>
                         <td>{{ _module.name }}</td>
                         <td v-if="!_module.noTD" :colspan="!_module.hasTP && 2">
-                            <input :class="_module.td.value > 20 && 'border-danger'" max="20" v-model="_module.td.value"  type="number" placeholder="TD" class="form-control border-none">
+                            <input max="20" min="0" :class="(_module.td.value > 20 || _module.td < 0) && 'border-danger'" max="20" v-model="_module.td.value"  type="number" placeholder="TD" class="form-control border-none">
                         </td>
                         <td v-if="_module.hasTP">
-                            <input :class="_module.tp.value > 20 && 'border-danger'" max="20" v-model="_module.tp.value"  type="number" placeholder="TP" class="form-control border-none">
+                            <input max="20" min="0" :class="(_module.tp.value > 20 || _module.tp < 0) && 'border-danger'" max="20" v-model="_module.tp.value"  type="number" placeholder="TP" class="form-control border-none">
                         </td>
                         <td :colspan="(_module.noTD && !_module.hasTP) ? 3 : 2">
-                            <input :class="_module.examen.value > 20 && 'border-danger'" max="20" v-model="_module.examen.value" type="number" placeholder="Examen" class="form-control border-none">
+                            <input max="20" min="0" :class="(_module.examen.value > 20 || _module.examen.value < 0) && 'border-danger'" max="20" v-model="_module.examen.value" type="number" placeholder="Examen" class="form-control border-none">
                         </td>
                     </tr>
                 </tbody>
