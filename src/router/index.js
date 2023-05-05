f36a3de2fd1a00617db4fea799cbb12d5eec1f49
Jugurtha Gaci <88148092+gcyugurten@users.noreply.github.com>
2023-05-05 08:14:19 +0200

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import L1 from './../pages/L1.vue'
 import L2 from './../pages/L2.vue'
 import L3 from './../pages/L3.vue'
@@ -6,38 +6,33 @@ import M1RO from "./../pages/M1RO.vue"
 import M2RO from "./../pages/M2RO.vue"
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     { 
       path: '/', 
       component: L1,
       name: 'licence/1',
-      props: true
     },
     { 
       path: '/licence/2', 
       component: L2,
       name: 'licence/2',
-      props: true
     },
     { 
       path: '/licence/3', 
       component: L3,
       name: 'licence/3',
-      props: true
 
     },
     { 
       path: '/master/ro/1', 
       component: M1RO,
       name: 'master/ro/1',
-      props: true
     },
     { 
       path: '/master/ro/2', 
       component: M2RO,
       name: 'master/ro/2',
-      props: true
     },
   ]
 })
